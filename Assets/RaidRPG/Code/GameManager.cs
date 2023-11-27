4910e0698fbc5cfa90022eeb581728bef4dc96d2
nix <code.03bb@gmail.com>
2023-11-27 17:49:59 +0100

    fix end game condition

--- Assets/RaidRPG/Code/GameManager.cs
+++ Assets/RaidRPG/Code/GameManager.cs
@@ -8,11 +8,13 @@ namespace RaidRPG
         [SerializeField] private Enemy _enemyPrefab;
         [SerializeField] private float _spawnRate = 2f;
         [SerializeField] private int _maxWave = 3;
+        [SerializeField] private float _waveDistance = 5f;
 
         private Player _player;
         private List<Enemy> _enemies = new();
         private Dictionary<int, int> _waves = new();
         private int _currentWave = 0;
+        private bool _finished;
 
         private void Awake()
         {
@@ -20,10 +22,13 @@ namespace RaidRPG
             //Populate wave dictionary
             PopulateWave();
             _currentWave = 0;
+            _finished = false;
         }
-        
+
         private void Update()
         {
+           if(_finished) return;
+           
             //Iterate on enemy list and remove dead enemies, if list is empty spawn next wave
             for (int i = _enemies.Count - 1; i >= 0; i--)
             {
@@ -34,6 +39,7 @@ namespace RaidRPG
                     _enemies.RemoveAt(i);
                 }
             }
+
             //Check if enemy list is empty
             if (_enemies.Count == 0)
             {
@@ -43,21 +49,26 @@ namespace RaidRPG
                     //Spawn next wave
                     SpawnWave();
                 }
+                else
+                {
+                    print("You win!");
+                    _finished = true;
+                    return;
+                }
+
                 //Increment current wave index
                 _currentWave++;
+              
             }
-            
-            if(_currentWave == _maxWave)
-            {
-                print("You win");
-            }
+
+           
         }
 
         //Spawn enemy at random position on a circle around player with random speed with a min distance from player
         public void SpawnEnemy()
         {
             //Get random position on a circle around player
-            Vector2 randomPosition = RandomCircle(_player.transform.position, 1.0f);
+            Vector2 randomPosition = RandomCircle(_player.transform.position, _waveDistance);
             //Get random speed
             float randomSpeed = Random.Range(1.0f, 5.0f);
             //Spawn enemy
@@ -78,7 +89,7 @@ namespace RaidRPG
                 _waves.Add(i, 2 * i + 1);
             }
         }
-        
+
         //Spawn wave of enemies based on current wave index
         private void SpawnWave()
         {
@@ -90,12 +101,6 @@ namespace RaidRPG
                 SpawnEnemy();
             }
         }
-        
-     
-        
-        
-        
-        
 
 
         //Get random position on a circle around center with a distance
